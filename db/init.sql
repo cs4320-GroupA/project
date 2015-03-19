7ca4c01cbe8f02be69d99fa248e691281dea7ec5
PDirks- <pld9bc@mail.missouri.edu>
2015-03-18 20:08:35 -0500

    skeleton for sql tables

---
+++ db/init.sql
@@ -0,0 +1,102 @@
+/*
+ *  init file for database tables
+ *  cs4320 - group A
+ *  March 17, 2015
+ */
+
+
+/*
+ * comments
+ *
+ */
+create table comments(
+    
+);
+
+/*
+ * user
+ *
+ */
+create table user(
+    
+);
+
+/*
+ * account type
+ *
+ */
+create table account_type(
+    
+);
+
+/*
+ * user
+ *
+ */
+create table user(
+    
+);
+
+/*
+ * form
+ *
+ */
+create table form(
+    
+);
+
+/*
+ * term (?)
+ *
+ */
+create table semester(
+    
+);
+
+/*
+ * status
+ *
+ */
+create table status(
+    
+);
+
+/*
+ * course
+ *
+ */
+create table course(
+    
+);
+
+/*
+ * form_data
+ *
+ */
+create table form_data(
+    
+);
+
+/*
+ * previous_taken
+ *
+ */
+create table previous_taken(
+    
+);
+
+/*
+ * previous_taught
+ *
+ */
+create table previous_taught(
+    
+);
+
+/*
+ * desired_courses
+ *
+ */
+create table desired_courses(
+    
+);

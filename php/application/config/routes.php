8a6f2a4d1115e08cd946c62a28f620aba1c50a41
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-03-31 22:45:49 -0500

    Testing codeignitor change view and homepage

--- php/application/config/routes.php
+++ php/application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "testing";
 $route['404_override'] = '';
 
 
---
+++ php/application/views/testing_message.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html lang="en">
+<head>Testing</head>
+<body>
+<img src="ostrich.jpg" alt="Smiley face">
+</body>
+</html>

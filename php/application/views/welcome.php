96cf6194847e0de42e75db9f31db5466de99cacc
masyv6 <masyv6@mail.missouri.edu>
2015-04-02 10:11:44 -0500

    Added registration button & modal.

--- php/application/views/welcome.php
+++ php/application/views/welcome.php
@@ -23,14 +23,15 @@
       <h1><b>Welcome to the Mizzou CS/IT TA & PLA Application Website</b></h1>
       <h3>Please sign in or register</h3>
       <br>
-    <button class="btn btn-primary btn-lg" href="#signup" data-toggle="modal" data-target=".bs-modal-sm">Log In</button>
+    <button class="btn btn-primary btn-lg" href="#" data-toggle="modal" data-target="#login">Log In</button>
+    <button class="btn btn-danger btn-lg" href="#" data-toggle="modal" data-target="#register">Register</button>
     </center>
     <br>
       <hr>
    </div>
     
   <!-- Modal -->
-    <div class="modal fade bs-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal fade bs-modal-sm" id="login" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-sm">
         <div class="modal-content">
           <div class="modal-header">
@@ -39,12 +40,12 @@
           <div class="modal-body">
             <div class="tab-pane fade active in" id="signin">
                 <form class="form-horizontal" action="#" method="POST">
-        <input type = "hidden" name = "action" value = "do_login">
+        <input type = "hidden" name = "action" value = "signin">
                 <fieldset>
                 <div class="control-group">
-                  <label class="control-label" for="userinput">Username:</label>
+                  <label class="control-label" for="pawprint">Pawprint:</label>
                   <div class="controls">
-                    <input required="" id="userinput" name="userinput" type="text" class="form-control" placeholder="username" class="input-medium" required="">
+                    <input required="" id="pawprint" name="pawprint" type="text" class="form-control" placeholder="abc123" class="input-medium" required="">
                   </div>
                 </div>
                 <div class="control-group">
@@ -67,7 +68,55 @@
         </div>
       </div>
     </div>
+
+    <div class="modal fade bs-modal-lg" id="register" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h3>Register</h3>
+          </div>
+          <div class="modal-body">
+            <div class="tab-pane fade active in" id="signin">
+                <form class="form-horizontal" action="#" method="POST">
+        <input type = "hidden" name = "action" value = "register">
+                <fieldset>
+                <div class="control-group">
+                  <label class="control-label" for="pawprint">Pawprint:</label>
+                  <div class="controls">
+                    <input required="" id="pawprint" name="pawprint" type="text" class="form-control" placeholder="abc123" class="input-medium" required="">
+                  </div>
+                </div>
+                <div class="control-group">
+                  <label class="control-label" for="passwordinput">Password:</label>
+                  <div class="controls">
+                    <input required="" id="passwordinput" name="passwordinput" class="form-control" type="password" placeholder="****" class="input-medium">
+                  </div>
+                </div>
+                <div class="container">
+                  <label class="control-label" for="accountRadio">Account Type:</label>
+                    <div class="radio">
+                      <label><input type="radio" id = "accountRadio" name = "accountRadio" value = "applicant">Applicant</label>
+                    </div>
+                    <div class="radio">
+                      <label><input type="radio" id = "accountRadio" name = "accountRadio" value = "instructor">Instructor</label>
+                    </div>
+                </div>
+                <!-- Button -->
+                <div class="control-group">
+                  <label class="control-label" for="signin"></label>
+                  <div class="controls">
+                    <button id="signin" name="signin" class="btn btn-success">Create Account</button>
+                  </div>
+                </div>
+                </fieldset>
+                </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
+

99d82addb67c7cf6e4c1ffdd05785212019f709b
wjbz82 <wjbz82@mail.missouri.edu>
2015-04-02 09:43:10 -0700

    doing small test to figure things out

--- php/application/views/testing_message.php
+++ php/application/views/testing_message.php
@@ -4,7 +4,7 @@
 <body>
 <?php
 $env_var = getenv('OPENSHIFT_HOMEDIR')."/app-root/repo/php/application/views/ostrich.jpg";
-echo '<img src="'.$env_var.'" alt="Ostrich">';
+echo '<img src="'.$env_var.'" alt="Ostrich">' . "test";
 ?>
 </body>
 </html>

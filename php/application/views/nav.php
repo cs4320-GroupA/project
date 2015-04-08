b26becce1c6f70b84d0edaa8de06107fb3c82af2
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-08 16:31:30 -0500

    Added missing model load in register and fix nav php issues

--- php/application/views/nav.php
+++ php/application/views/nav.php
@@ -15,20 +15,21 @@
           <ul class="nav navbar-nav">
 			<!-- The buttons on the navbar change depending on who the user is logged in as-->
 			<?php
-					//codeigniter grabbing the user type from the current sessions
-					$user_type = $this->session->userdata('user_type');
-					//check if 'user_type' is set
-					if($this->session->userdata('user_type')) {
-						if(strcmp($user_type,"applicant")) { 
-							echo "<li><a href='#'>Applicant Profile</a></li>";
-						//Nav options for instructor
-						} else if(strcmp($user_type,"instructor")) {
+				//check if 'user_type' is set
+				if($this->session->userdata('user_type')) {
+					if($this->session->userdata('user_type') == 'applicant'){ 
+						echo "<li><a href='#'>Applicant Profile</a></li>";
+					//Nav options for instructor
+					} else if($this->session->userdata('user_type') == 'instructor'){
 						echo "<li><a href='#'>Instructor Profile</a></li>";
-						//Nav options for admin
-						} else if(strcmp($user_type,"admin")) {
-							echo "<li><a href='#'>Admin profile</a></li>";
-						} 
-					}
+					//Nav options for admin
+					} else if($this->session->userdata('user_type') == 'admin'){
+						echo "<li><a href='#'>Admin profile</a></li>";
+					} 
+				}
+				else {
+					redirect('login', 'refresh');
+				}			
 			?>
           </ul>
         </div>

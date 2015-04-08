ea15de442d83656e4e13cbda5f764124897f1ced
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-08 15:16:14 -0500

    Fixed more mySQL syntax

--- php/application/models/user_model.php
+++ php/application/models/user_model.php
@@ -24,7 +24,13 @@ class User_model extends CI_Model {
 	}
 
 	public function register($username, $password, $salt, $account_type) {
-		$sql = 'INSERT INTO user (username, password, salt, account_type) VALUES (?, ?, ?, (SELECT account_type_id FROM tasub.account_type WHERE account_type_name = ?))';
-		return($this->db->query($sql, array($username, $password, $salt, $account_type));
+		$sql = 'INSERT INTO user (username, password, salt, account_type) VALUES (?, ?, ?, ?)';
+		$type_sql = 'SELECT account_type_id FROM tasub.account_type WHERE account_type_name = ?';
+		
+		$query = $this->db->query($type_sql, array($account_type));
+		$type_id = $query->row()->account_type_id;
+		$result = $this->db->query($sql, array($username, $password, $salt, $type_id);
+
+		return $result;
 	}
 ?>

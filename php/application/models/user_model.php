7ad6621774c2767153886d64be9154e79203f3bd
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-08 15:10:50 -0500

    Fixed mySQL syntax

--- php/application/models/user_model.php
+++ php/application/models/user_model.php
@@ -24,7 +24,7 @@ class User_model extends CI_Model {
 	}
 
 	public function register($username, $password, $salt, $account_type) {
-		$sql = 'INSERT INTO user (username, password, salt, account_type) VALUES (?, ?, ?, SELECT account_type_id FROM tasub.account_type WHERE account_type_name = ?)';
+		$sql = 'INSERT INTO user (username, password, salt, account_type) VALUES (?, ?, ?, (SELECT account_type_id FROM tasub.account_type WHERE account_type_name = ?))';
 		return($this->db->query($sql, array($username, $password, $salt, $account_type));
 	}
 ?>

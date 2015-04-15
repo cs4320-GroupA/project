12f54ed7bc52548aeae919cde58412496bc73b8e
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-15 17:57:13 -0500

    Added remainging form data to database

--- db/init.sql
+++ db/init.sql
@@ -39,6 +39,8 @@ create table form(
     submission_date date not null,
     form_data int references form_data( form_data_id ),
     user_id int references user(user_id),
+    signature varchar(50),
+    signature_date varchar(10),
     primary key( form_id, semester_id )
 )character set 'utf8';
 
@@ -54,11 +56,17 @@ create table form_data(
     student_id varchar(10) not null,
     assistant_type varchar(3) not null,
     expected_graduation varchar(50) not null,
-    SPEAK_OPT_score float not null,
-    advisor varchar(50) not null,
+    SPEAK_OPT_score float, 
+    advisor varchar(50),
     gpa float not null,
     phone_number int,
-    last_date_of_test date not null,
+    last_date_of_test varchar(20),
+    grad_type varchar(4),
+    other_work varchar(30),
+    gato boolean,
+    speak_assessment boolean,
+    onita boolean,
+    signature varchar(50),
     primary key( form_data_id )
 )character set 'utf8';
 
@@ -91,7 +99,7 @@ DROP TABLE IF EXISTS course;
 create table course(
     course_id int auto_increment,
     course_name varchar(50),
-    semester varchar(10),
+    semester varchar(20),
     instructor_id int references user(user_id),
     primary key( course_id, course_name )
 )character set 'utf8';
@@ -110,7 +118,7 @@ create table comments(
 )character set 'utf8';
 
 --
---	Many to many relationship tables...
+--  Many to many relationship tables...
 --
  
 /*
@@ -119,53 +127,52 @@ create table comments(
 DROP TABLE IF EXISTS previous_taken;
 DROP TABLE IF EXISTS currently_teaching; 
 create table currently_teaching(
-	currently_teaching_id int auto_increment,
-	course_id int references course( course_id ),
-	course_name varchar(50) references course( course_name ),
-	form_data_id int references form_data( form_data_id ),
-	primary key( currently_teaching_id, course_id, course_name, form_data_id )
+    currently_teaching_id int auto_increment,
+    course_id int references course( course_id ),
+    course_name varchar(50) references course( course_name ),
+    form_data_id int references form_data( form_data_id ),
+    primary key( currently_teaching_id, course_id, course_name, form_data_id )
 )character set 'utf8'; 
 
 DROP TABLE IF EXISTS previous_taught; 
 create table previous_taught(
-	previous_taught_id int auto_increment,
-	course_id int references course( course_id ),
-	course_name varchar(50) references course( course_name ),
-	form_data_id int references form_data(form_data_id),
-	primary key( previous_taught_id, course_id, course_name, form_data_id )
+    previous_taught_id int auto_increment,
+    course_id int references course( course_id ),
+    course_name varchar(50) references course( course_name ),
+    form_data_id int references form_data(form_data_id),
+    primary key( previous_taught_id, course_id, course_name, form_data_id )
 )character set 'utf8';
 
 DROP TABLE IF EXISTS desired_courses; 
 create table desired_courses(
-	desired_course_id int auto_increment,
-	course_id int references course( course_id ),
-	course_name varchar(50) references course( course_name ),
-	form_data_id int references form_data( form_data_id ),
+    desired_course_id int auto_increment,
+    course_id int references course( course_id ),
+    course_name varchar(50) references course( course_name ),
+    form_data_id int references form_data( form_data_id ),
     grade float not null,
-	primary key( desired_course_id, course_id, course_name, form_data_id )
+    primary key( desired_course_id, course_id, course_name, form_data_id )
 )character set 'utf8'; 
  
 /*
- *	form and course tables...
+ *  form and course tables...
  */
 DROP TABLE IF EXISTS assigned_courses; 
 create table assigned_courses(
-	assigned_id int auto_increment,
-	course_id int references course( course_id ),
-	course_name varchar(50) references course( course_name ),
-	form_id int references form( form_id ),
-	semester_id int references semester( semester_id ),
-	primary key( assigned_id, course_id, course_name, form_id, semester_id )
+    assigned_id int auto_increment,
+    course_id int references course( course_id ),
+    course_name varchar(50) references course( course_name ),
+    form_id int references form( form_id ),
+    semester_id int references semester( semester_id ),
+    primary key( assigned_id, course_id, course_name, form_id, semester_id )
 ) character set 'utf8'; 
 
 DROP TABLE IF EXISTS course_preference; 
 create table course_preference(
-	preference_id int auto_increment,
-	course_id int references course( course_id ),
-	course_name varchar(50) references course( course_name ),
-	form_id int references form( form_id ),
-	semester_id int references semester( semester_id ),
-	preference_number int,
-	primary key( preference_id, course_id, course_name, form_id, semester_id )
+    preference_id int auto_increment,
+    course_id int references course( course_id ),
+    course_name varchar(50) references course( course_name ),
+    form_id int references form( form_id ),
+    semester_id int references semester( semester_id ),
+    preference_number int,
+    primary key( preference_id, course_id, course_name, form_id, semester_id )
 ) character set 'utf8';
-

cd90f665f1a63283d7fbcb76bc0171eb1e6bf0f5
masyv6 <masyv6@mail.missouri.edu>
2015-04-16 12:39:02 -0500

    Adding status_title to the jumbotron after the last push

--- php/application/views/infopage.php
+++ php/application/views/infopage.php
@@ -11,7 +11,7 @@
 				echo '<div class="jumbotron">';
 					echo '<center>';
 					echo '<h1>Welcome!</h1>';
-					echo '<p>The timeline is currently in the following window: '.$this->session->userdata("status_id").'*WAITING FOR SESSION VAR*</p>';
+					echo '<p>The timeline is currently in the following window: '.$this->session->userdata("status_title").'</p>';
 					echo '<p>Current Semester: '.$this->session->userdata("semester_title").'</p>';
 					echo '</center>';
 				echo '</div>';

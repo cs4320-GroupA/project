8d40e25b298c74ce356c1df3e86c705f166994f9
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-15 23:05:11 -0500

    Fixed navbar logout link

--- php/application/views/nav.php
+++ php/application/views/nav.php
@@ -37,7 +37,7 @@
 					redirect('login', 'refresh');
 				}			
 			?>
-		<li><a href='login/logout'>Logout</a></li>
+		<li><a href='<?php echo base_url(); ?>index.php/login/logout'>Logout</a></li>
           </ul>
         </div>
       </div>

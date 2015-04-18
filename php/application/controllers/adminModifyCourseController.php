1671fc88bd70872ce9be3957a847ae1db1367ccc
Robert Stovall <rochovian@gmail.com>
2015-04-18 00:29:28 -0500

    some more skeleton work..

--- php/application/controllers/adminModifyCourseController.php
+++ php/application/controllers/adminModifyCourseController.php
@@ -10,5 +10,28 @@
 		{ 
 			$this->load->view('adminModifyCourse');
 		}
-	}
+
+		public function populate(){
+			$this->load->model('Course_model');
+			$this->load->model('Semester_model');
+
+			
+		}
+
+		public function remove(){
+			$this->load->model('Course_model');
+			$this->load->model('Semester_model');
+
+		}
+
+		public function add(){
+			$this->load->model('Course_model');
+			$this->load->model('Semester_model');
+
+		}
+
+		public function modify(){
+			$this->load->model('Course_model');
+			$this->load->model('Semester_model');
+		}
 ?>

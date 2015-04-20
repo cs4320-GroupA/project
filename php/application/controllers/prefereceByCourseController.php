d1f8e873db0230283996f0fd13ca248aad363561
PDirks- <pld9bc@mail.missouri.edu>
2015-04-20 12:54:56 -0500

    created add() function

--- php/application/controllers/prefereceByCourseController.php
+++ php/application/controllers/prefereceByCourseController.php
@@ -10,6 +10,28 @@ class preferenceByCourse extends CI_Controller {
 		$this->load->view('preferenceByCourse');
 	}// end index
     
+    public function add(){
+    
+        $course_id = htmlspecialchars( $_POST['course_id'] );
+        
+        $form_id = htmlspecialchars( $_POST['form_id'] );
+        
+        $course_name = htmlspecialchars( $_POST['course_name'] );
+        
+        // not sure if semester_id has a default for this project
+        $semester_id = htmlspecialchars( $_POST['semester_id'] );
+        
+        $pref_number = htmlspecialchars( $_POST['pref_number'] );
+        
+        $result = $this->Course_model->createPreference( $course_id, $course_name, $form_id, $semester_id, $preference_number );
+        
+        /*
+         * do something with $result ? some kind of error check?
+         */
+        
+        
+    }// end add function
+    
 }// end class
 
 ?>

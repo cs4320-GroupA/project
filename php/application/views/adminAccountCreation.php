230f62949a7029aec35469ed750fbefcea3036ca
masyv6 <masyv6@mail.missouri.edu>
2015-04-21 00:28:31 -0500

    Adding form for admin to add more admin accounts

--- php/application/views/adminAccountCreation.php
+++ php/application/views/adminAccountCreation.php
@@ -21,6 +21,27 @@
 				<h2>Account Creation</h2>
 			</div>
 		</div>
+		<div class="container">
+			<form class="form-horizontal">
+				<div class="form-group">
+					<label for="adminID" class="col-sm-2 control-label">Admin Pawprint</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="adminID" placeholder="Admin Pawprint">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="adminPassword" class="col-sm-2 control-label">Password</label>
+					<div class="col-sm-10">
+						<input type="password" class="form-control" id="adminPassword" placeholder="Password">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<button type="submit" class="btn btn-success">Register a New Admin Account</button>
+					</div>
+				</div>
+			</form>
+		</div>
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>

65dae485891ff836aad38e9720cb9bfbf87eb85d
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-21 12:59:51 -0500

    Inital push for inserting currently teaching courses

---
+++ php/application/models/currently_teaching.php
@@ -0,0 +1,19 @@
+<?php 
+    class Currently_Teaching_model extends CI_Model {
+        function __construct(){
+            parent::__construct();
+        }
+
+        public function insert($course_id, $course_name, $form_data_id) {
+            $sql = 'INSERT INTO currently_teaching(course_id, course_name, form_data_id) VALUES (?, ?, ?)';
+
+            $this->db=>query($sql, array($course_id, $course_name, $form_data_id));
+
+            if($this->db->affected_rows() == 1) {
+                return TRUE;
+            } else {
+                return FALSE;
+            }
+        }
+    }
+?>

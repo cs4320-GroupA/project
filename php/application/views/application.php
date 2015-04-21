c6c871c456bd121214b505d1883f60dd117d778c
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-20 21:30:08 -0500

    Last styling push

--- php/application/views/application.php
+++ php/application/views/application.php
@@ -23,7 +23,7 @@
 
     		function addCurrentlyRow(form) {
     			currently_count++;
-    			var new_row = '<p id="currently_row'+currently_count+'"><select class="form-control" name = "currently_teaching[]">'+getCourses()+'/select>  <input type="button" class="btn btn-success" onclick="removeCurrentlyRow('+currently_count+');" value="Remove"></p>';
+    			var new_row = '<p id="currently_row'+currently_count+'"><select class="form-control" name = "currently_teaching[]">'+getCourses()+'</select> <input type="button" class="btn btn-success" onclick="removeCurrentlyRow('+currently_count+');" value="Remove"></p>';
     			$('.currently_wrapper').append(new_row);
     		}
 
@@ -43,7 +43,7 @@
     		
     		function addDesiredRow(form) {
     			desired_count++;
-    			var new_row = '<p id="desired_row'+desired_count+'"><select class="form-control" name = "currently_teaching[]">'+getCourses()+'</select> ';
+    			var new_row = '<p id="desired_row'+desired_count+'"><select class="form-control" name = "desired_teaching[]">'+getCourses()+'</select> ';
     			new_row += ' <select class="form-control" name = "gradeReceived[]"> \
 								<option>A+</option> \
 								<option>A</option> \

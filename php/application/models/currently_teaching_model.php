e7c1f338727c76dee62682b1268ef42d5277ca2f
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-21 13:06:16 -0500

    Fixed syntax error

--- php/application/models/currently_teaching_model.php
+++ php/application/models/currently_teaching_model.php
@@ -7,7 +7,7 @@
         public function insert($course_id, $course_name, $form_data_id) {
             $sql = 'INSERT INTO currently_teaching(course_id, course_name, form_data_id) VALUES (?, ?, ?)';
 
-            $this->db=>query($sql, array($course_id, $course_name, $form_data_id));
+            $this->db->query($sql, array($course_id, $course_name, $form_data_id));
 
             if($this->db->affected_rows() == 1) {
                 return TRUE;

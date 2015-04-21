96f444507e644496e62432df5a8966b348fd079a
Robert Stovall <rochovian@gmail.com>
2015-04-21 12:17:17 -0500

    Syntax errors

--- php/application/controllers/adminModifyCourseController.php
+++ php/application/controllers/adminModifyCourseController.php
@@ -3,18 +3,20 @@
 	{
 		public function __construct() 
 		{
+			
+			$this->load->model('Course_model');
+			$this->load->model('Semester_model');
             		parent::__construct();
+			$this->adminModifyCourseController->index();
        		 }
 
 		public function index() 
 		{ 
-			$this->load->model('Course_model');
 			$courses = $this->Course_model->getCourses();
 			$this->load->view('adminModifyCourse',$courses);
 		}
 
 		public function remove(){
-			$this->load->model('Course_model');
 
 			$course_id = htmlspecialchars($_POST['course_id']);
 
@@ -25,8 +27,6 @@
 		}
 
 		public function add(){
-			$this->load->model('Course_model');
-			$this->load->model('Semester_model');
 			
     			$course_id = htmlspecialchars($_POST['course_id']);
     			$course_name = htmlspecialchars($_POST['course_name']);

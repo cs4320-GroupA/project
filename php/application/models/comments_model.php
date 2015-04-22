293d48efb255731e56a8f1e19e0ece2f59c88e7b
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-22 16:48:05 -0500

    Fixed syntax

--- php/application/models/comments_model.php
+++ php/application/models/comments_model.php
@@ -7,7 +7,7 @@
         public function insert($posted_about_id, $posted_by_id, $score, $description) {
             $sql = 'INSERT into tasub.comments(posted_by, posted_about, score, description) VALUES(?, ?, ?, ?)';
 
-            $query = $this->db->query($sql, array($posted_by_id, $posted_about_id, $score, $description))
+            $query = $this->db->query($sql, array($posted_by_id, $posted_about_id, $score, $description));
         }
     }
 ?>

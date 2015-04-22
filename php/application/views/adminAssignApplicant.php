8185c2ab755fccd6022f94d1564e75f01006c8ba
masyv6 <masyv6@mail.missouri.edu>
2015-04-22 01:24:17 -0500

    Shell of applicant assignment view

--- php/application/views/adminAssignApplicant.php
+++ php/application/views/adminAssignApplicant.php
@@ -21,6 +21,87 @@
 				<h2>Assign Applicants</h2>
 			</div>
 		</div>
+		<div class="container">
+			<div class='row'>
+				<div class="col-md-4">
+					<label for="courseToAssign">Course</label>
+					<select class="form-control" name = "courseToAssign" required>
+						<option>a</option>
+						<option>b</option>
+						<option>c</option>
+						<option>d</option>
+						<option>e</option>
+						<option>f</option>
+						<option>g</option>
+						<option>h</option>
+						<option>i</option>
+						<option>j</option>
+					</select>
+				</div>
+			</div>
+			<br><br>
+			<div class='row'>
+				<div class="col-md-6">
+					<h3>Applicant Pool</h3>
+					<table class="table table-hover table-striped">
+						<thead>
+							<tr>
+								<th>Assign</th>
+								<th>Pawprint</th>
+								<th>GPA</th>
+								<th>Course Grade</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td><input type="checkbox"></td>
+								<td>masyv6</td>
+								<td>3.875</td>
+								<td>A-</td>
+							</tr>
+							<tr>
+								<td><input type="checkbox"></td>
+								<td>ats314</td>
+								<td>3.245</td>
+								<td>A+</td>
+							</tr>
+							<tr>
+								<td><input type="checkbox"></td>
+								<td>pws326</td>
+								<td>2.587</td>
+								<td>C+</td>
+							</tr>
+						</tbody>
+					</table>
+					<button type = "submit" class = "btn btn-success">Assign Selected Applicants</button>
+				</div>
+				<div class="col-md-6">
+					<h3>Instructor Preferences</h3>
+					<table class="table table-hover table-striped">
+						<thead>
+							<tr>
+								<th>Pawprint</th>
+								<th>Comments</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>masyv6</td>
+								<td>Great TA -- would love him back this semester</td>
+							</tr>
+							<tr>
+								<td>ats314</td>
+								<td>Never again</td>
+							</tr>
+							<tr>
+								<td>pws326</td>
+								<td>Solid TA -- strong technical ability, low effort in the classroom</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>

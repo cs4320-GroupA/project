4d9e108cfb51ee417acc2424383bb887bc6e9882
masyv6 <masyv6@mail.missouri.edu>
2015-04-22 15:01:27 -0500

    Removing links to fonts

--- php/application/views/infopage.php
+++ php/application/views/infopage.php
@@ -1,6 +1,12 @@
-!DOCTYPE html
-<html>
+<!DOCTYPE html>
+<html lang = "en">
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="../../favicon.ico">
 		<title>Home</title>
 		<link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
 	</head>

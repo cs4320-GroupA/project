0cad0b24790a988ed42b7ab297459b3e91524bca
PDirks- <pld9bc@mail.missouri.edu>
2015-04-22 22:21:10 -0500

    added <form> tags

--- php/application/views/adminTemporalModification.php
+++ php/application/views/adminTemporalModification.php
@@ -37,7 +37,9 @@
 							echo 'The application timeline is currently in the <b>Application mode</b>. Here, applicants can submit applications to be a TA/PLA for the upcoming semester. Upon moving the application to the Selection mode, no applications will be allowed to be submitted and instructors will begin to be able to preference applicants.';
 							echo '</p>';
 							//echo '<button type="button" class="btn btn-primary">Move Forward to Selection</button>';
+                            echo "\n<form>\n";
                             echo '<button type="submit" class="btn btn-primary" formaction="'.base_url().'index.php/adminTemporalModificationController/set/2>Move Forward to Selection</button>';
+                            echo "\n<form>";
 						echo '</div>';
 					}// end application if
 					else if ($this->session->userdata("status_title") == "SELECTION")
@@ -46,8 +48,9 @@
 							echo '<p>';
 							echo 'The application timeline is currently in the <b>Selection mode</b>. Here, instructors will preference applicants for each of their classes. Upon moving back to the Application mode, applicant submissions are reopened.';
 							echo '</p>';
-							//echo '<button type="button" class="btn btn-primary">Move Back to Application</button>';
+							echo '<form>';
                             echo '<button type="submit" class="btn btn-primary" formaction="'.base_url().'index.php/adminTemporalModificationController/set/1>Move Back to Application</button>';
+                            echo '</form>';
 						echo '</div>';
 						echo '<div class ="col-md-6">';
 							echo '<p>';
@@ -62,13 +65,17 @@
 							echo '<p>';
 							echo 'The application timeline is currently in the <b>Notification mode</b>. Here, instructors will preference applicants for each of their classes. Upon moving the application to the Selection mode, no applications will be allowed to be submitted and instructors will begin to be able to preference applicants.';
 							echo '</p>';
+                            echo "\n<form>\n";
                             echo '<button type="submit" class="btn btn-primary" formaction="'.base_url().'index.php/adminTemporalModificationController/set/2>Move Back to Selection</button>';
+                            echo "\n</form>";
 						echo '</div>';
 						echo '<div class ="col-md-6">';
 							echo '<p>';
 							echo 'The application timeline is currently in the <b>Notification mode</b>. Here, instructors will preference applicants for each of their classes. Upon moving back to the Application mode, applicant submissions are reopened.';
 							echo '</p>';
+                            echo '<form>';
                             echo '<button type="submit" class="btn btn-primary" formaction="'.base_url().'index.php/adminTemporalModificationController/set/1>Move Back to Application</button>';
+                            echo '</form>';
 						echo '</div>';
 					}// end notification else if
 					else
@@ -78,20 +85,26 @@
 							echo 'Move to the Application mode. Here, applicants can submit applications to be a TA/PLA for the upcoming semester. Upon moving the application to the Selection mode, no applications will be allowed to be submitted and instructors will begin to be able to preference applicants.';
 							echo '</p>';
 							//echo '<button type="button" class="btn btn-primary">Move to Application</button>';
+                            echo '<form>';
                             echo '<button type="submit" class="btn btn-primary" formaction="'.base_url().'index.php/adminTemporalModificationController/set/1>Move to Application</button>';
+                            echo '</form>';
 						echo '</div>';
 						echo '<div class ="col-md-4">';
 							echo '<p>';
 							echo 'Move to the Selection mode. Here, instructors will preference applicants for each of their classes. Upon moving to the Notification mode, applicants are notified of their potential appointment as a TA/PLA of a course.';
 							echo '</p>';
+                            echo '<form>';
                             echo '<button type="submit" class="btn btn-primary" formaction="'.base_url().'index.php/adminTemporalModificationController/set/2>Move to Selection</button>';
+                            echo '</form>';
 						echo '</div>';
 						echo '<div class ="col-md-4">';
 							echo '<p>';
 							echo 'Move to the Notification mode. Here, the applicants are notified of their potential appointment as a TA/PLA of a course.';
 							echo '</p>';
 							//echo '<button type="button" class="btn btn-primary">Move to Notification</button>';
+                            echo '<form>';
                             echo '<button type="submit" class="btn btn-primary" formaction="'.base_url().'index.php/adminTemporalModificationController/set/3>Move to Notification</button>';
+                            echo '</form>';
 						echo '</div>';
 					}// end closed else if
 				?>				

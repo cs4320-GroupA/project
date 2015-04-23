5ee3a337ae19e6b5ced034bb5fc631bbd5046cbf
PDirks- <pld9bc@mail.missouri.edu>
2015-04-22 21:19:45 -0500

    changed buttons to a href among other things

--- php/application/views/adminTemporalModification.php
+++ php/application/views/adminTemporalModification.php
@@ -21,67 +21,84 @@
 		</div>
 		<div class="container">
 			<div class="row">
-				<?php 
+				<?php
+
+                /*
+                 * need to give out different user options based on current semester status
+                 * if:  application - can go to selection (2)
+                 *      selection - can go to application or to notification (1, 3)
+                 *      notification - can go to application or to selection (1, 2)
+                 *      closed - can go to any of the first three (1, 2, 3)
+                 */
 					if ($this->session->userdata("status_title") == "APPLICATION")
 					{
 						echo '<div class ="col-md-12">';
 							echo '<p>';
 							echo 'The application timeline is currently in the <b>Application mode</b>. Here, applicants can submit applications to be a TA/PLA for the upcoming semester. Upon moving the application to the Selection mode, no applications will be allowed to be submitted and instructors will begin to be able to preference applicants.';
 							echo '</p>';
-							echo '<button type="button" class="btn btn-primary">Move Forward to Selection</button>';
+							//echo '<button type="button" class="btn btn-primary">Move Forward to Selection</button>';
+                        // might need to use 's href' for the function calls here
+                            echo '<a href='.base_url().'index.php/adminTemporalModificationController/set/2>Move Forward to Selection</a>';
 						echo '</div>';
-					}
+					}// end application if
 					else if ($this->session->userdata("status_title") == "SELECTION")
 					{
 						echo '<div class ="col-md-6">';
 							echo '<p>';
 							echo 'The application timeline is currently in the <b>Selection mode</b>. Here, instructors will preference applicants for each of their classes. Upon moving back to the Application mode, applicant submissions are reopened.';
 							echo '</p>';
-							echo '<button type="button" class="btn btn-primary">Move Back to Application</button>';
+							//echo '<button type="button" class="btn btn-primary">Move Back to Application</button>';
+                            echo '<a href='.base_url().'index.php/adminTemporalModificationController/set/1>Move Back to Application</a>';
 						echo '</div>';
 						echo '<div class ="col-md-6">';
 							echo '<p>';
 							echo 'The application timeline is currently in the <b>Selection mode</b>. Here, instructors will preference applicants for each of their classes. Upon moving to the Notification mode, applicants are notified of their potential appointment as a TA/PLA of a course.';
 							echo '</p>';
-							echo '<button type="button" class="btn btn-primary">Move Forward to Notification</button>';
+							//echo '<button type="button" class="btn btn-primary">Move Forward to Notification</button>';
+                            echo '<a href='.base_url().'index.php/adminTemporalModificationController/set/3>Move Forward to Notification</a>';                  
 						echo '</div>';
-					}
+					}// end selection else if
 					else if ($this->session->userdata("status_title") == "Notification")
 					{
 						echo '<div class ="col-md-6">';
 							echo '<p>';
 							echo 'The application timeline is currently in the <b>Notification mode</b>. Here, instructors will preference applicants for each of their classes. Upon moving the application to the Selection mode, no applications will be allowed to be submitted and instructors will begin to be able to preference applicants.';
 							echo '</p>';
-							echo '<button type="button" class="btn btn-primary">Move Back to Selection</button>';
+							//echo '<button type="button" class="btn btn-primary">Move Back to Selection</button>';
+                            echo '<a href='.base_url().'index.php/adminTemporalModificationController/set/2>Move Back to Selection</a>';
 						echo '</div>';
 						echo '<div class ="col-md-6">';
 							echo '<p>';
 							echo 'The application timeline is currently in the <b>Notification mode</b>. Here, instructors will preference applicants for each of their classes. Upon moving back to the Application mode, applicant submissions are reopened.';
 							echo '</p>';
-							echo '<button type="button" class="btn btn-primary">Move Back to Application</button>';
+							//echo '<button type="button" class="btn btn-primary">Move Back to Application</button>';
+                            echo '<a href='.base_url().'index.php/adminTemporalModificationController/set/1>Move Back to Application</a>';
 						echo '</div>';
-					}
+					}// end notification else if
 					else
 					{
 						echo '<div class ="col-md-4">';
 							echo '<p>';
 							echo 'Move to the Application mode. Here, applicants can submit applications to be a TA/PLA for the upcoming semester. Upon moving the application to the Selection mode, no applications will be allowed to be submitted and instructors will begin to be able to preference applicants.';
 							echo '</p>';
-							echo '<button type="button" class="btn btn-primary">Move to Application</button>';
+							//echo '<button type="button" class="btn btn-primary">Move to Application</button>';
+                            echo '<a href='.base_url().'index.php/adminTemporalModificationController/set/1>Move to Application</a>';
 						echo '</div>';
 						echo '<div class ="col-md-4">';
 							echo '<p>';
 							echo 'Move to the Selection mode. Here, instructors will preference applicants for each of their classes. Upon moving to the Notification mode, applicants are notified of their potential appointment as a TA/PLA of a course.';
 							echo '</p>';
-							echo '<button type="button" class="btn btn-primary">Move to Selection</button>';
+							//echo '<button type="button" class="btn btn-primary">Move to Selection</button>';
+                            echo '<a href='.base_url().'index.php/adminTemporalModificationController/set/2>Move to Selection</a>';
 						echo '</div>';
 						echo '<div class ="col-md-4">';
 							echo '<p>';
 							echo 'Move to the Notification mode. Here, the applicants are notified of their potential appointment as a TA/PLA of a course.';
 							echo '</p>';
-							echo '<button type="button" class="btn btn-primary">Move to Notification</button>';
+							//echo '<button type="button" class="btn btn-primary">Move to Notification</button>';
+                            echo '<a href='.base_url().'index.php/adminTemporalModificationController/set/3>Move to Notification</a>';
 						echo '</div>';
-					}
+					}// end closed else if
 				?>				
 			</div>
 		</div>

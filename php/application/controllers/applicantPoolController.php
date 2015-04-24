40728c8fc6f6ddc41d22666eba3d9e95adb6a6d8
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-24 14:27:18 -0500

    Fixing syntax

--- php/application/controllers/applicantPoolController.php
+++ php/application/controllers/applicantPoolController.php
@@ -10,8 +10,10 @@ class applicantPoolController extends CI_Controller {
        $this->load->model('form_model');
        $this->load->model('form_data_model');
        $this->load->model('semester_model');
-       
-       $data['applicants'] = $this->form_model->getAllBySemesterId(($this->semester_model->getCurrentSemester())->row()->semester_id);
+
+       $semester = $this->semester_model->getCurrentSemester();
+
+       $data['applicants'] = $this->form_model->getAllBySemesterId($semester->row()->semester_id);
        
 	   $this->load->view('applicant_pool', $data);
     }// end index

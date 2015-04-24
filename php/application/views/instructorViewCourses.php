7a59d6c4fd54c67aeb593df0635ec1f443e8a646
JamesL13 <jmlmdf@mail.missouri.edu>
2015-04-23 22:10:04 -0500

    Fix error

--- php/application/views/instructorViewCourses.php
+++ php/application/views/instructorViewCourses.php
@@ -46,7 +46,6 @@
 				</div><!--col-md-12-->
 			</div><!--row-->
 		</div><!--container-->
-
 		
 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   	 <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>

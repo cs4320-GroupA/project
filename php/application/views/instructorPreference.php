fb4f9734c58a717d9095c1380fb6c3b60e722c59
masyv6 <masyv6@mail.missouri.edu>
2015-04-30 16:54:58 -0500

    Typos

--- php/application/views/instructorPreference.php
+++ php/application/views/instructorPreference.php
@@ -65,7 +65,7 @@
 							if(isset($desired_forms)) {
   								foreach($desired_forms as $row) {
 									echo '<tr>';
-				    				echo '<td">';
+				    				echo '<td>';
 				    				echo '<form>';
 									echo '<button type="submit" class="btn btn-default" formaction="'.base_url().'index.php/form/viewForm/'.$row->user_id.'/'.$row->semester_id.'"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> View</button> ';
 					    			echo '</td>';

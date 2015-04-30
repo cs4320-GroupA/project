e466dbc2c319389a563e79eadafe7ee959144a97
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-30 12:10:33 -0500

    Correct function calls

--- php/application/controllers/home.php
+++ php/application/controllers/home.php
@@ -8,8 +8,8 @@
 		public function index() {
 			$this->load->model('semester_model');
 
-			$data['status_title'] = $this->semester_model->getCurrentSemesterTitle();
-			$data['semester_title'] = $this->semester_model->getCurrentSemesterStatus();
+			$data['status_title'] = $this->semester_model->getCurrentSemesterStatus();
+			$data['semester_title'] = $this->semester_model->getCurrentSemesterTitle();
 			
 			$this->load->view('infopage', $data);
 		}

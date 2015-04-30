5328b8e270acdd4723905e906f6ae7d3ddf9ac17
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-04-30 15:54:59 -0500

    Fixed table name

--- php/application/models/course_model.php
+++ php/application/models/course_model.php
@@ -209,7 +209,7 @@
      * input:   $course_id - course to query
      */
         public function getPreferenceByCourse( $course_Id ){
-            $q = 'SELECT * FROM tasub.course_preferences INNER JOIN tasub.form ON tasub.form.form_id = tasub.course_preferences.form_id INNER JOIN tasub.form_data ON tasub.form_data.form_data_id = tasub.form.form_data WHERE tasub.course_preference.course_id = ?';     
+            $q = 'SELECT * FROM tasub.course_preference INNER JOIN tasub.form ON tasub.form.form_id = tasub.course_preference.form_id INNER JOIN tasub.form_data ON tasub.form_data.form_data_id = tasub.form.form_data WHERE tasub.course_preference.course_id = ?';     
             $result = $this->db->query( $q, array( $course_Id ) );
             
             if( $result->num_rows() > 0 ){

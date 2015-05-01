20abba9c370059ba9321dcda0d8dea56ed238754
masyv6 <masyv6@mail.missouri.edu>
2015-04-30 19:05:53 -0500

    Adding bootstrap javascript

--- php/application/views/navTest.php
+++ php/application/views/navTest.php
@@ -30,3 +30,12 @@
     </div>
   </div><!-- /container -->
 </div><!-- /navbar wrapper -->
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>

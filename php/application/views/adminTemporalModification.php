651d2958dff8815a5dcc066db0f1a8736dbd4a6c
masyv6 <masyv6@mail.missouri.edu>
2015-05-03 22:08:13 -0500

    Attempting to fix alignment

--- php/application/views/adminTemporalModification.php
+++ php/application/views/adminTemporalModification.php
@@ -24,20 +24,24 @@
 		</div>
 		<div class="container">
 				<?php
-						echo '<div class="jumbotron timeline">';
-							echo '<div class = "col-md-6">';
-								echo '<p><strong>Current Window: </strong>'.$status_title.'</p>';
-							echo '</div>';
-							echo '<div class = "col-md-6">';
-								echo '<p><strong>Current Semester: </strong>'.$semester_title.'</p>';
+						echo '<div class = "row">';
+							echo '<div class="jumbotron timeline">';
+								echo '<div class = "col-md-6">';
+									echo '<p><strong>Current Window: </strong>'.$status_title.'</p>';
+								echo '</div>';
+								echo '<div class = "col-md-6">';
+									echo '<p><strong>Current Semester: </strong>'.$semester_title.'</p>';
+								echo '</div>';
 							echo '</div>';
 						echo '</div>';
 						echo '<div class = "row">';
 							echo '<div class = "col-md-12">';
 								echo '<h3>Move this Semester\'s Timeline</h3>';
 							echo '</div>';
+						echo '</div>';
 					if ($status_title == "APPLICATION")
 					{
+						echo '<div class = "row">';
 							echo '<div class ="col-md-12">';
 								echo '<p>';
 								echo 'The application timeline is currently in the <b>Application mode</b>. Here, applicants can submit applications to be a TA/PLA for the upcoming semester. Upon moving the application to the Selection mode, no applications will be allowed to be submitted and instructors will begin to be able to preference applicants.';
@@ -50,6 +54,7 @@
 					}
 					else if ($status_title == "SELECTION")
 					{
+						echo '<div class = "row">';
 							echo '<div class ="col-md-6">';
 								echo '<p>';
 								echo 'The application timeline is currently in the <b>Selection mode</b>. Here, instructors will preference applicants for each of their classes. Upon moving back to the Application mode, applicant submissions are reopened.';
@@ -70,6 +75,7 @@
 					}
 					else if ($status_title == "NOTIFICATION")
 					{
+						echo '<div class = "row">';
 							echo '<div class ="col-md-6">';
 								echo '<p>';
 								echo 'The application timeline is currently in the <b>Notification mode</b>. Here, instructors will preference applicants for each of their classes. Upon moving the application to the Selection mode, no applications will be allowed to be submitted and instructors will begin to be able to preference applicants.';
@@ -90,6 +96,7 @@
 					}
 					else
 					{
+						echo '<div class = "row">';
 							echo '<div class ="col-md-4">';
 								echo '<p>';
 								echo 'Move to the Application mode. Here, applicants can submit applications to be a TA/PLA for the upcoming semester. Upon moving the application to the Selection mode, no applications will be allowed to be submitted and instructors will begin to be able to preference applicants.';
@@ -116,23 +123,22 @@
 							echo '</div>';
 						echo'</div>';
 					}
-
 					echo'<div class="row">';
 						  echo'<div class="col-md-12">';
 						    echo'<h3>Change Semester</h3>';
 						  echo'</div>';
 					echo'</div>';
-						echo'<div class="row">';
-						  echo'<div class="col-md-6">';
-						  echo '<form class="form-inline" name="changeSemester" action="'.base_url().'index.php/adminTemporalModificationController/changeSemester" method="POST">';
+					echo'<div class="row">';
+						echo'<div class="col-md-6">';
+							echo '<form class="form-inline" name="changeSemester" action="'.base_url().'index.php/adminTemporalModificationController/changeSemester" method="POST">';
 						    echo'<div class="col-md-2">';
-						      echo'<label>Select Semester: </label>';
+						    	echo'<label>Select Semester: </label>';
 						    echo'</div>';
 						    echo'<div class="col-md-2">';
-						      echo'<label>Semester</label>';
+						    	echo'<label>Semester</label>';
 						    echo '</div>';
 						    echo '<div class = "col-md-8">';
-						      echo'<select class = "form-control" name="changeTo" required>';
+						    	echo'<select class = "form-control" name="changeTo" required>';
 						        echo'<option disabled hidden selected></option>';
 
 						       	if(isset($semesters)) {
@@ -142,19 +148,19 @@
 						       	}
 
 						    	echo'</select>';
-						    echo '</div>';
-						    echo '<div class="col-md-12">';
-						      echo '<button type="submit" id = "timelineBtn" class="btn btn-default"><span class="glyphicon glyphicon-check" aria-hidden="true"></span> Select</button>';
-						    echo '</div>';
-						    echo '</form>';
+							echo '</div>';
+				    		echo '<div class="col-md-12">';
+				    			echo '<button type="submit" id = "timelineBtn" class="btn btn-default"><span class="glyphicon glyphicon-check" aria-hidden="true"></span> Select</button>';
+				    		echo '</div>';
+				    		echo '</form>';
 						  echo '</div>';
 						  echo '<div class="col-md-6">';
 						  echo '<form class="form-inline" name="newSemester" action="'.base_url().'index.php/adminTemporalModificationController/newSemester" method="POST">';
 						    echo '<div class="col-md-2">';
-						      echo '<label>New Semester: </label>';
+						    	echo '<label>New Semester: </label>';
 						    echo '</div>';
 						    echo '<div class="col-md-2 col-md-1-offset">';
-						      echo '<label>Season </label>';
+						    	echo '<label>Season </label>';
 						    echo '</div>';
 						    echo '<div class="col-md-4">';
 						      echo '<select name="newTitle" class = "form-control" required>';
@@ -164,17 +170,17 @@
 						    	echo '</select>';
 						    echo '</div>';
 						    echo '<div class="col-md-1">';
-						      echo '<label>Year </label>';
+						    	echo '<label>Year </label>';
 						    echo '</div>';
 						    echo '<div class="col-md-2">';
-						      echo '<input type = "text" name="newYear" class = "form-control" placeholder = "2020" required>';
+						    	echo '<input type = "text" name="newYear" class = "form-control" placeholder = "2020" required>';
 						    echo '</div>';
 						    echo '<div class="col-md-12">';
-						      echo '<button type="submit" id = "timelineBtn" class="btn btn-default"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add</button>';
+						    	echo '<button type="submit" id = "timelineBtn" class="btn btn-default"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add</button>';
 						    echo '</div>';
-						  echo '</div>';
-						  echo '</form>';
-						echo '</div>';	
+						echo '</div>';
+						echo '</form>';
+					echo '</div>';	
 				?>
 		</div>
 		

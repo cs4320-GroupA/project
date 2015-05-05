e8ec92e603b6aaf4688da921424678cb04820502
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-05-05 18:45:35 -0500

    Fixed syntax

--- php/application/models/form_data_model.php
+++ php/application/models/form_data_model.php
@@ -36,7 +36,7 @@
         
             //Return TRUE on success, FALSE on failure
             if($this->db->affected_rows() > 0) {
-                $sql = 'SELECT * FROM tasub.form_data WHERE first_name = ? AND last_name = ? AND mizzou_email = ? AND student_id = ? AND assistant_type = ? AND semester = ?)';
+                $sql = 'SELECT * FROM tasub.form_data WHERE first_name = ? AND last_name = ? AND mizzou_email = ? AND student_id = ? AND assistant_type = ? AND semester = ?';
 
                 $query = $this->db->query($sql, array($fname, $lname, $email, $studentID, $asstType, $semester));
 

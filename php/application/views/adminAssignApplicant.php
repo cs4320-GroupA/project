c6f7db889c2e4165ada0a391561500bb03065dd3
Brendan Niebruegge <bgnqp4@mail.missouri.edu>
2015-05-06 01:34:46 -0500

    Remove ability to remove assigned applicants when in notification/applicants stage

--- php/application/views/adminAssignApplicant.php
+++ php/application/views/adminAssignApplicant.php
@@ -83,7 +83,9 @@
 			    <table class="table table-hover table-striped">
 			      <thead>
 			        <tr>
+			        <?php if(isset($assignable)) { ?>
 			          <th>Remove</th>
+			        <?php } ?>
 			          <th>Full Name</th>
 			          <th>GPA</th>
 			          <th>Grad Year</th>
@@ -95,7 +97,9 @@
                         foreach($assigned_applicants as $row) {
                             echo '<tr>'; 
                             echo '<form>';
+                            if(isset($assignable)) {
                                 echo '<td><button class="btn btn-default" formaction="'.base_url().'index.php/adminAssignApplicantController/remove/'.$row->assigned_id.'/'.$currentCourse->course_id.'"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Remove</button>';
+                            }
                                 echo "<td>" . $row->first_name . " " . $row->last_name . "</td>"; 
                                 echo "<td>" . $row->gpa . "</td>";
                                 echo "<td>" . $row->expected_graduation . "</td>";
